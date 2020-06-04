c46eac74fc3c69e4527ba7419ff911c6e0d1f56f
Bradley Jacinto <bradleyivme@gmail.com>
2020-06-04 13:10:55 -0600

    Se agregó el Label junto al Entry de la entrada NNombre

--- exa.py
+++ exa.py
@@ -11,4 +11,9 @@ root.title('Examen Final')
 Magua = Label(text="GitHub: Bradleyivme",font=("Agency FB",8)).place(x=20,y=10)
 Saludo = Label(text="Bienvenido",font=("Agency FB",25)).place(x=150,y=20)
 
+NNombre = Label(text="Nombre:",font=("Agency FB", 14)).place(x=125,y=80)
+#Creando la entrada de texto de Nombre
+entradaN = StringVar()
+txtUsuario = Entry(root,textvariable=entradaN).place(x=180,y=88)
+
 root.mainloop()
