9d0b9a685667eff91c5f5e0910ffe469c9f83132
Bradley Jacinto <bradleyivme@gmail.com>
2020-06-04 13:06:33 -0600

    Se agregó la marca de agua y el título principal

--- exa.py
+++ exa.py
@@ -8,4 +8,7 @@ alto = 400
 root.geometry(str(ancho)+'x'+str(alto))
 root.title('Examen Final')
 
+Magua = Label(text="GitHub: Bradleyivme",font=("Agency FB",8)).place(x=20,y=10)
+Saludo = Label(text="Bienvenido",font=("Agency FB",25)).place(x=150,y=20)
+
 root.mainloop()
